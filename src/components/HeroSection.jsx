05748eef9bd195d135c3fdee14028d946a138ba2
Curdarious Bell <BellCurdarious@yahoo.com>
2022-06-06 02:51:35 -0500

    Created Hero Section Component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className="flex items-center justify-center flex-col py-20">
+           <div className="text-center">
+            <h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-blue-600 font-semibold">Hero Section</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+           </div> 
+        </div>
+    )
+}
+
+export default HeroSection;
